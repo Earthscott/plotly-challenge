c035f32bc945e2b275ceead1b9685fcf3a5e235b
Scott Job <earthscott@gmail.com>
2021-10-18 11:14:13 -0400

    Completed project

--- static/js/app.js
+++ static/js/app.js
@@ -40,9 +40,22 @@ function procDemogData(demogData) {
     };
 };
 
-// Return top 10 OTUs for selected subId
-function top10(selSample) {
-    console.log([...Array(selSample.length).keys()]);
+function updatePlots(subId, sampleData) {
+    // Bar chart
+    Plotly.restyle("bar", "x", [sampleData["sample_values"].slice(0,10)]);
+    Plotly.restyle("bar", "y", [sampleData["otu_id_text"].slice(0,10)]);
+    Plotly.restyle("bar", "text", [sampleData["otu_labels"].slice(0,10)]);
+    Plotly.relayout("bar", "title", `Top 10 Operational Taxonomic Units (OTUs) by<br> Relative Abundance for Test Subject ${subId}`);
+
+    // Bubble chart
+    Plotly.restyle("bubble", "x", [sampleData["otu_ids"]]);
+    Plotly.restyle("bubble", "y", [sampleData["sample_values"]]);
+    Plotly.restyle("bubble", "text", [sampleData["otu_labels"]]);
+    Plotly.restyle("bubble", "marker", {
+        size: sampleData["sample_values"].map(k => k * 0.65),
+        color: sampleData["otu_ids"],
+        colorscale: "Portland"
+    });
 }
 
 function init() {
@@ -64,25 +77,59 @@ function init() {
     // call Demographics card function with metadata Object for initial subject id
     procDemogData(metaLookup[names[0]]);
 
-    // create Object to store samples data by subject id, sorted reverse by 
-    // OTU sample values
+    // create Object to store samples data by subject id, 
+    // sorted reverse by OTU sample values
     let sampleLookup = {};
     for (let i = 0; i < samples.length; i++) {
-        // let subID = samples[i]["id"];
         let obj = samples[i];
-        let arr = [];
-        for (let j = 0; j < obj["otu_ids"].length; j++) {
-            arr.push([obj["otu_ids"][j], obj["sample_values"][j], obj["otu_labels"][j]]);
-        }
-        // arr.sort((a,b) => a[1].localeCompare(b[1]));
-        // sampleLookup[obj.id] = arr;
-    }
+        let arr = [obj.otu_ids, obj.sample_values, obj.otu_labels];
+        arr.sort((a,b) => b[1] - a[1]);
+        sampleLookup[obj.id] = {
+            "otu_ids": arr[0],
+            "otu_id_text": arr[0].map(j => "OTU " + j),
+            "sample_values": arr[1],
+            "otu_labels": arr[2]
+        };
+    };
 
-    // get samples Object with top 10 OTUs for selected subId
-    // top10(samples[names[0]]);
+    // Create bar chart using samples Object for initial subject id
+    let barData = [{
+        x: sampleLookup[names[0]]["sample_values"].slice(0,10),
+        y: sampleLookup[names[0]]["otu_id_text"].slice(0,10),
+        text: sampleLookup[names[0]]["otu_labels"].slice(0,10),
+        hoverinfo: "text",
+        orientation: "h",
+        type: "bar"
+    }];
+    let barLayout = {
+        title: `Top 10 Operational Taxonomic Units (OTUs) by<br> Relative Abundance for Test Subject ${names[0]}`,
+        yaxis: {autorange: "reversed"},
+        xaxis: {title: "Total Number of Reads (Proxy for Relative Abundance)"},
+        height: 600,
+        margin: {"pad": 10}
+    };
+    Plotly.newPlot("bar", barData, barLayout);
+    
+    // Create bubble chart using samples Object for initial subject id
+    let bubbleData = [{
+        x: sampleLookup[names[0]]["otu_ids"],
+        y: sampleLookup[names[0]]["sample_values"],
+        mode: "markers",
+        marker: {size: sampleLookup[names[0]]["sample_values"].map(k => k * 0.65),
+                 color: sampleLookup[names[0]]["otu_ids"],
+                 colorscale: "Portland"},
+        text: sampleLookup[names[0]]["otu_labels"],
+        hoverinfo: "text"
+    }];
+    let bubbleLayout = {
+        title: "Bubble Chart of Total Number of Reads versus OTU IDs<br>" +
+               "<sub>Bubble Radius is proportional to the Total Number of Reads</sub>",
+        xaxis: {title: "OTU IDs"},
+        yaxis: {title: "Total Number of Reads"}
+    };
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-
-    return (metaLookup);
+    return [metaLookup, sampleLookup];
 };
 
 // On change to the DOM, call getSubId()
@@ -99,6 +146,9 @@ function getSubId() {
     // selected subject id
     procDemogData(demogLookup[subId]);
 
+    // Update plots
+    updatePlots(subId, sampleLookup[subId]);
+
 };
 
-demogLookup = init();
+[demogLookup, sampleLookup] = init();
